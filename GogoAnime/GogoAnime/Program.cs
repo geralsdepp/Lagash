389a45720187f6cc1f4446e88d9e1df4c29a7ef2
geralsdepp <geraldinemeza23@gmail.com>
2018-01-27 02:22:22 -0300

    Add files via upload

---
+++ GogoAnime/GogoAnime/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+
+namespace GogoAnime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+        }
+    }
+}
---
+++ GogoAnime/PluginGogoAnime/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PluginGogoAnime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.WriteLine("Se requiere un unico parametro 'baseUrl'");
+                Console.WriteLine(args.Length);
+                return;
+            }
+            
+            string url = args[0];
+
+            IPlugin plugin;
+
+            if (url.Contains(url))
+            {
+                plugin = new Plugin(url);
+            }
+            else
+            {
+                throw new NotSupportedException("URL no soportada");
+            }
+            
+
+            plugin.ListarVideosDisponibles("https://www.commoncraft.com/videolist");
+            byte[] b = plugin.Bajar();
+            //BinaryWriter writer = new BinaryWriter(
+            //writer.Write(b);
+        }
+    }
+}
